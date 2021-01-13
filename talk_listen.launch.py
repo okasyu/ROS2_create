e73884d69c2a1a0ba219c15e2b67ca7bc30a1838
okasyu <72854059+okasyu@users.noreply.github.com>
2021-01-13 11:06:09 +0900

    Update talk_listen.launch.py

--- talk_listen.launch.py
+++ talk_listen.launch.py
@@ -14,4 +14,4 @@ def generate_launch_description():
             node_executable = 'listener',
             output = 'screen'
             )
-    return launch.LaunchDescription([talker, listener])                                                                                                                      ~                                                                                                                       ~                                                                                                                       ~                             
+    return launch.LaunchDescription([talker, listener])   
