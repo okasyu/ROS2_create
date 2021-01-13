3901fd65cb44fa8fa02ec06064607162e880e8de
okasyu <72854059+okasyu@users.noreply.github.com>
2021-01-13 11:03:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,30 +2,29 @@ from setuptools import setup
 import os
 from glob import glob
 
-package_name = 'mypkg'
+package_name = 'ROS2_create'
 
 setup(
-            name=package_name,
-                version='0.0.0',
-                    packages=[package_name],
-                        data_files=[
-                                    ('share/ament_index/resource_index/packages',
-                                                    ['resource/' + package_name]),
-                                            ('share/' + package_name, ['package.xml']),
-                                                    (os.path.join('share', package_name), glob('launch/*.launch.py'))
-                                                        ],
-                            install_requires=['setuptools'],
-                                zip_safe=True,
-                                    maintainer='okasyu',
-                                        maintainer_email='okasyun919@gmail.com',
-                                            description='a package for ROS2_create',
-                                                license='BSD',
-                                                    tests_require=['pytest'],
-                                                        entry_points={
-                                                                    'console_scripts': [
-                                                                                    'talker = mypkg.talker:main',
-                                                                                                'listener = mypkg.listener:main',
-                                                                                                        ],
-                                                                        },
-                                                        )
-~                                                                                                                                                                                            ~                              
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='okasyu',
+    maintainer_email='okasyun919@gmail.com',
+    description='a package for ROS2_create',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
+        ],
+    },
+)                                                                                                                     
